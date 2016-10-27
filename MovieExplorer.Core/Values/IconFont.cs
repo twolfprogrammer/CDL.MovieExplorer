1eb0f7b00b581c48a0cf4a22a9583b7e9f334958
Thomas Wolf <thomas@wolfprogrammer.com>
2016-10-27 00:13:23 -0600

    [Core, iOS] adds Icon Font

---
+++ MovieExplorer.Core/Values/IconFont.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MovieExplorer.Core.Values {
+	public static class IconFont {
+		public const string Name = "MovieExplorer";
+		public const string IconStarEmpty = "\xe900";
+		public const string IconStarFull = "\xe901";
+		public const string IconClose = "\xe902";
+	}
+}
