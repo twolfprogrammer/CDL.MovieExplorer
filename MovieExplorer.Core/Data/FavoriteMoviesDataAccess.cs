296f540b1deb95ab7be8e1af3313bb6741a34d15
Thomas Wolf <thomas@wolfprogrammer.com>
2016-10-27 00:06:00 -0600

    [Core] adds Favorite Movies storage (via SQLite)

---
+++ MovieExplorer.Core/Data/FavoriteMoviesDataAccess.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace MovieExplorer.Core {
+	public class FavoriteMoviesDataAccess {
+
+		SQLiteDatabase database;
+
+		static FavoriteMoviesDataAccess instance;
+		internal static FavoriteMoviesDataAccess Instance {
+			get {
+				if (instance == null) {
+					throw new Exception(Values.Errors.FavoriteMovieServiceNotInitialized);
+				}
+				return instance;
+			}
+		}
+
+		private FavoriteMoviesDataAccess() { }
+
+		internal static void Initialize(ISQLite sqlite) {
+			instance = new FavoriteMoviesDataAccess();
+			instance.database = new SQLiteDatabase(Values.SQLite.DatabaseName, sqlite);
+			instance.database.CreateTable<FavoriteMovie>();
+		}
+
+		public bool SaveMovie(int movieId, string title) {
+			if (!ContainsMovie(movieId)) {
+				database.SaveItem<FavoriteMovie>(new FavoriteMovie(movieId, title));
+			}
+			return ContainsMovie(movieId);
+		}
+
+		public bool RemoveMovie(int movieId) {
+			var movie = FindMovie(movieId);
+			if (movie != null) {
+				database.DeleteItem<FavoriteMovie>(movie.ID);
+			}
+			return !ContainsMovie(movieId);
+		}
+
+		public bool ContainsMovie(int movieId) {
+			var movie = FindMovie(movieId);
+			return movie != null && movie.MovieId == movieId;
+		}
+
+		FavoriteMovie FindMovie(int movieId) {
+			return database.FindWithQuery<FavoriteMovie>(
+				Values.SQLite.SelectFavoriteMovieByIdQuery,
+				new object[] { movieId });
+		}
+	}
+}
---
+++ MovieExplorer.Core/Models/SQLite/FavoriteMovie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MovieExplorer.Core {
+	public class FavoriteMovie : SQLiteBaseItem {
+		public int MovieId { get; set; }
+		public string Title { get; set; }
+		public FavoriteMovie() { }
+		public FavoriteMovie(int movieId, string title) {
+			MovieId = movieId;
+			Title = title;
+		}
+	}
+}
