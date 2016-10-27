0fc1f690c327d09661e76494c898a1ea2808c53e
Thomas Wolf <thomas@wolfprogrammer.com>
2016-10-26 23:57:15 -0600

    [iOS] adds SQLite support

---
+++ MovieExplorer.iOS/Data/SQLite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using MovieExplorer.Core;
+using SQLite;
+
+namespace MovieExplorer.iOS {
+	public class SQLite : ISQLite {
+		string GetPath(string databaseName) {
+			if (string.IsNullOrWhiteSpace(databaseName)) {
+				throw new Exception(Core.Values.Errors.InvalidDatabaseFilename);
+			}
+			var sqliteFilename = $"{databaseName}.{Core.Values.SQLite.FilenameExtension}";
+			var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			var libraryPath = Path.Combine(documentsPath, "..", "Library");
+			var path = Path.Combine(libraryPath, sqliteFilename);
+			return path;
+		}
+
+		public SQLiteConnection GetConnection(string databaseName) {
+			return new SQLiteConnection(GetPath(databaseName));
+		}
+	}
+}
